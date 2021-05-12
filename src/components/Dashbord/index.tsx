046e5d6ab36cefd2c7caf65b0d28fde60ed93009
Relancio Borges dos Santos <relancio.dev@gmail.com>
2021-05-12 13:50:36 -0300

    Adicionado Tabela de Transações

--- src/components/Dashbord/index.tsx
+++ src/components/Dashbord/index.tsx
@@ -1,10 +1,12 @@
 import Summary from "../Summary/index";
+import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export default function Dashboard (){
     return(
         <Container>
             <Summary/>
+            <TransactionsTable/>
         </Container>
     )
 }
